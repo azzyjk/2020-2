5b9afbb7442468b78a9cfeefd1835c2a8efa8414
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-10-04 23:45:01 +0900

    main : 년/월 선택안하고 나왔을 때도 "일지 불러오기 프롬프트" 실행되는거 수정

--- ComputerEngineeringProject/Code/main.py
+++ ComputerEngineeringProject/Code/main.py
@@ -9,7 +9,7 @@ from diary import *
 user = None
 task = None
 loadDiaryTask = None
-YYYYMMDD = None
+
 os.system("clear")
 
 while user==None:
@@ -27,25 +27,29 @@ while user==None:
 
 
 while user!=None and task!="0":
+    YYYYMMDD = None
+    
     showMenu(mainMenu)
     task=input("메뉴를 선택해주세요")
+
     if task == '1':
         writeDiary(user)
     elif task == "2":
         YYYYMMDD = loadDiary(user)
-        # 일지 불러오기 프롬프트
-        showMenu(loadDairyMenu)
-        loadDiaryTask = input()
-        if loadDiaryTask =='1':
-            editDiary(user, YYYYMMDD)
-            pass
-        elif loadDiaryTask == '2':
-            delDiary(user,YYYYMMDD)
-            pass
-        elif loadDiaryTask == '0':
-            continue
-        else:
-            print("존재하지 않는 메뉴를 선택했습니다. 다시 입력해주세요.")
+        if YYYYMMDD != None:
+            # 일지 불러오기 프롬프트
+            showMenu(loadDairyMenu)
+            loadDiaryTask = input()
+            if loadDiaryTask =='1':
+                editDiary(user, YYYYMMDD)
+                pass
+            elif loadDiaryTask == '2':
+                delDiary(user,YYYYMMDD)
+                pass
+            elif loadDiaryTask == '0':
+                continue
+            else:
+                print("존재하지 않는 메뉴를 선택했습니다. 다시 입력해주세요.")
 
     elif task == '3':
         pass
@@ -58,3 +62,5 @@ while user!=None and task!="0":
         print("잘못 선택했습니다. 다시 선택해주세요.")
         
 print("프로그램을 종료합니다.")
+
+# DD를 해서 4는 안되고 04만 된다고 해놓기0
