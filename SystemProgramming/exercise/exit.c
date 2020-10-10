e398e70bd1f8b7aee2757de537bfbd1e4ef71ee4
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-10-10 16:10:42 +0900

    exit : add header

--- SystemProgramming/exercise/exit.c
+++ SystemProgramming/exercise/exit.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<unistd.h> // fork function
 #include<stdlib.h> // exit function
+#include<sys/wait.h> // wait function
 
 int main(){
     int stat_loc=0;
 
-    fork();
     fork();
     wait(&stat_loc);
     printf("pid : %ld, %d\n", (long)getpid(), WEXITSTATUS(stat_loc));
