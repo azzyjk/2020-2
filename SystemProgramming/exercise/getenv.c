9f50f8f475fc9eb1bcb8e46256c871c15a987962
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-10-10 16:19:15 +0900

    getenv : add description

--- SystemProgramming/exercise/getenv.c
+++ SystemProgramming/exercise/getenv.c
@@ -3,6 +3,6 @@
 
 int main(){
     char* env = "PWD";
-    printf("%s\n",getenv(env)); // 없으면 NULL을 준다.
+    printf("%s\n",getenv(env)); // 없으면 NULL을 준다., ubuntu에서는 null이 아니라 segment fault가 뜬다.
     return 0;
 }
