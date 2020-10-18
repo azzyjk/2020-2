dab637caefdff88db93e2bcafd6278b205159bb9
azzyjk <azzyjk@naver.com>
2020-10-18 14:39:51 +0000

    lseek : test lseek over the file size wirte

--- SystemProgramming/exercise/lseek.c
+++ SystemProgramming/exercise/lseek.c
@@ -2,17 +2,25 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<stdio.h>
+#include<string.h>
 
 #define READ_FLAGS O_RDONLY
+#define BOTH_FLAGS O_RDWR
+
+#define WRITE_PERM ()
+
 #define BUF_SIZE 3
+
 int main(){
     int fd;
     char buf[BUF_SIZE];
 
-    fd = open("test2.txt", READ_FLAGS);
-    lseek(fd, 1, SEEK_CUR);
+    fd = open("test2.txt", BOTH_FLAGS);
+    lseek(fd, 15, SEEK_CUR);
     printf("%d\n", fd);
-    read(fd, buf, BUF_SIZE);
-    printf("%s\n", buf);
+    // read(fd, buf, BUF_SIZE);
+    strcpy(buf, "hi");
+    write(fd, buf, BUF_SIZE);
+    // printf("%s\n", buf);
     return 0;
 }
