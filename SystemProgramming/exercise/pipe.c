3d05ca3e3f9f1cf11c2be72f92b7285b43fbe7ca
azzyjk <azzyjk@naver.com>
2020-10-31 04:32:12 +0000

    pipe : practice pipe with using fork

--- SystemProgramming/exercise/pipe.c
+++ SystemProgramming/exercise/pipe.c
@@ -1,6 +1,8 @@
 #include<unistd.h>
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include<sys/wait.h>
 
 #define MAXLEN 1000
 
@@ -8,12 +10,23 @@ int main(){
     int pipefd[2];
     char buf[1000];
     char* str;
-    
-    str = "Hello";
+    pid_t childPid;
 
     pipe(pipefd);
-    write(pipefd[1], str, strlen(str));
-    read(pipefd[0], buf, MAXLEN);
+
+    childPid = fork();
+
+    if(childPid){
+        while(wait(NULL)!=-1){
+            read(pipefd[0], buf, MAXLEN);
+        }
+    }
+    else{
+        sleep(10);
+        str = "Hello";
+        write(pipefd[1], str, strlen(str));
+        exit(0);
+    }
     
     printf("%s\n", buf);
     return 0;
